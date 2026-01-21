6ca737b44161d2462f3b911976036df0ee721b97
micheleferri01 <micheleferri095@gmail.com>
2026-01-21 15:47:11 +0100

    soluzione snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,7 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+console.table(nums);
+
+const evenNums = nums.filter((num) => num % 2 === 0);
+console.table(evenNums);

3b383ee8494ae398a115e54b5b93f417bbb8b3a8
micheleferri01 <micheleferri095@gmail.com>
2026-01-21 15:21:27 +0100

    soluzione snack-2

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,8 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+people.forEach((person) => console.log(person.name));
+
+// soluzione con scomposizione dell'oggetto
+people.forEach(({name}) => console.log(name));

970fa8d364987994d7a26daaac990970e9a689a9
micheleferri01 <micheleferri095@gmail.com>
2026-01-21 16:00:20 +0100

    soluzione snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+console.table(students);
+
+const foundStudent = students.find(({id}) => id === 2);
+console.log(foundStudent);
